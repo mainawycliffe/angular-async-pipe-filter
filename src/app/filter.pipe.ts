2cf1676656c67e7be47088ae9a635827a861b6b2
Maina Wycliffe <wmmaina7@gmail.com>
2018-12-13 17:57:28 +0300

    build docs dir for github pages

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -8,7 +8,7 @@ import { Country, FilterService } from './filter.service';
 export class FilterPipe implements PipeTransform {
   constructor(private filterService: FilterService) {}
 
-  transform(value: string, searchText: string): Observable<Country[]> {
-    return this.filterService.filterCountries(searchText);
+  transform(value: any, filterTerm: string): Observable<Country[]> {
+    return this.filterService.filterCountries(filterTerm);
   }
 }
